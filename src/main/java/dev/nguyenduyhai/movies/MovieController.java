95a43ec2263b6fd0e37f49aaf09adc11c645da99
Hải Nguyễn Duy <nguyenduyhai0018@gmail.com>
2023-09-16 23:19:15 +0700

    update

--- src/main/java/dev/nguyenduyhai/movies/MovieController.java
+++ src/main/java/dev/nguyenduyhai/movies/MovieController.java
@@ -4,10 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
 import java.util.List;
@@ -16,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "*")
 public class MovieController {
 
     @Autowired
